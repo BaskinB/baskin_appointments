5d0da2e6d99410a8303c8b6b75dc0402594ea7c5
BaskinB <54458253+BaskinB@users.noreply.github.com>
2025-10-02 13:49:09 -0500

    feat: Added a version checker

--- .github/actions/bump-manifest-version.js
+++ .github/actions/bump-manifest-version.js
@@ -1,11 +1,8 @@
 const fxManifest = await Bun.file('./fxmanifest.lua').text();
-const versionFile = await Bun.file('./version').text();
 
 let newVersion = process.env.TGT_RELEASE_VERSION;
-newVersion = newVersion.replace(/^v/, '');
+newVersion = newVersion.replace('');
 
 const newFileContent = fxManifest.replace(/\bversion\s+(.*)$/gm, `version '${newVersion}'`);
-const versionContent = versionFile.replace(/<.*>/, `<${newVersion}>`);
 
 await Bun.write('./fxmanifest.lua', newFileContent);
-await Bun.write('./version', versionContent);
--- server/helpers/functions.lua
+++ server/helpers/functions.lua
@@ -12,8 +12,47 @@
 VORPcore = exports.vorp_core:GetCore()     -- NEW includes  new callback system
 BccUtils = exports['bcc-utils'].initiate() -- Initalize BccUtils
 -- Version Checker
-local repo = 'https://github.com/BaskinB/baskin_appointments'
-BccUtils.Versioner.checkRelease(GetCurrentResourceName(), repo)
+local function versionCheck(repository)
+  local resource = GetInvokingResource() or GetCurrentResourceName()
+
+  local currentVersion = GetResourceMetadata(resource, 'version', 0)
+
+  if currentVersion then
+    currentVersion = currentVersion:match('%d+%.%d+%.%d+')
+  end
+
+  if not currentVersion then return print(("^1Unable to determine current resource version for '%s' ^0"):format(resource)) end
+
+  SetTimeout(1000, function()
+    PerformHttpRequest(('https://api.github.com/repos/%s/releases/latest'):format(repository), function(status, response)
+      if status ~= 200 then return end
+
+      response = json.decode(response)
+      if response.prerelease then return end
+
+      local latestVersion = response.tag_name:match('%d+%.%d+%.%d+')
+      if not latestVersion or latestVersion == currentVersion then return end
+
+      local cv = { string.strsplit('.', currentVersion) }
+      local lv = { string.strsplit('.', latestVersion) }
+
+      for i = 1, #cv do
+        local current, minimum = tonumber(cv[i]), tonumber(lv[i])
+
+        if current ~= minimum then
+          if current < minimum then
+            return print(('^3An update is available for %s (current version: %s)\r\n%s^0'):format(resource,
+              currentVersion, response.html_url))
+          else
+            break
+          end
+        end
+      end
+    end, 'GET')
+  end)
+end
+
+versionCheck('BaskinB/baskin_appointments')
 
 -- Credits to Jake2k4 for this snippet.
 CreateThread(function()
